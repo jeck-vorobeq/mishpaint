ecf06cf1f82a1cb9d644262e747f2bfbb29f4667
jeck-vorobeq <m.a.karasev2011@gmale.com>
2021-12-25 09:58:00 +0300

    ?

--- movement.py
+++ movement.py
@@ -1,17 +1,17 @@
-import wrap,main
+import wrap
 
 @wrap.on_key_down()
 def management(keys):
-
+    pla = wrap.sprite.add("pacman", 100, 600, "player2")
     if wrap.K_w in keys:
-        wrap.sprite.set_angle(main.pla, 0)
-        wrap.sprite.move(main.pla, 0, -5)
+        wrap.sprite.set_angle(pla, 0)
+        wrap.sprite.move(pla, 0, -5)
     if wrap.K_s in keys:
-        wrap.sprite.set_angle(main.pla, 180)
-        wrap.sprite.move(main.pla, 0, 5)
+        wrap.sprite.set_angle(pla, 180)
+        wrap.sprite.move(pla, 0, 5)
     if wrap.K_a in keys:
-        wrap.sprite.set_angle(main.pla, -90)
-        wrap.sprite.move(main.pla, -5, 0)
+        wrap.sprite.set_angle(pla, -90)
+        wrap.sprite.move(pla, -5, 0)
     if wrap.K_d in keys:
-        wrap.sprite.set_angle(main.pla, 90)
-        wrap.sprite.move(main.pla, 5, 0)
+        wrap.sprite.set_angle(pla, 90)
+        wrap.sprite.move(pla, 5, 0)
