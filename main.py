f165ff11238bbf81e0debde52f377bbb407fc3d4
jeck-vorobeq <m.a.karasev2011@gmale.com>
2021-12-25 19:50:41 +0300

    ?

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import wrap
 
-import movement
-
-wrap.world.create_world(800,700)
-wrap.world.set_back_color(100,200,200)
+wrap.world.create_world(800, 700)
+wrap.world.set_back_color(100, 200, 200)
 wrap.world.set_title("MAZE")
-pla=wrap.sprite.add("pacman",100,600,"player2")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+wrap.sprite.add("img", 100, 600,"img")
+pla = wrap.sprite.add("pacman", 100, 600, "player2")
--- movement.py
+++ movement.py
@@ -1,8 +1,11 @@
 import wrap
 
+pla = wrap.sprite.add("pacman", 100, 600, "player2")
+
+
 @wrap.on_key_down()
 def management(keys):
-    pla = wrap.sprite.add("pacman", 100, 600, "player2")
+    global pla
     if wrap.K_w in keys:
         wrap.sprite.set_angle(pla, 0)
         wrap.sprite.move(pla, 0, -5)
