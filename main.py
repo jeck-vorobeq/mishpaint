854a136d4b5f8f7681d8de3983cd6c7abbf789f8
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-01-19 09:09:19 +0300

    ?

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import wrap,time
-
+import wrap, time
 
 wrap.add_sprite_dir("img")
-wrap.world.create_world(800, 700)
+wrap.world.create_world(900, 700)
 wrap.world.set_back_color(100, 200, 200)
 wrap.world.set_title("MAZE")
-pla = wrap.sprite.add("pacman", 100, 600, "player2")
-wrap.sprite.add("img", 100, 600,"img")
+pla = wrap.sprite.add("pacman", -12, 0, "player2")
+wrap.sprite.add("img", 400, 350, "img")
 import movement
--- movement.py
+++ movement.py
@@ -1,20 +1,20 @@
 import wrap
 
-pla = wrap.sprite.add("pacman", 100, 600, "player2")
+pla = wrap.sprite.add("pacman", 20, 90, "player2")
 
 
-@wrap.on_key_always(wrap.K_w,wrap.K_s,wrap.K_a,wrap.K_d)
+@wrap.on_key_always(wrap.K_w, wrap.K_s, wrap.K_a, wrap.K_d)
 def management(keys):
     global pla
     if wrap.K_w in keys:
         wrap.sprite.set_angle(pla, 0)
-        wrap.sprite.move(pla, 0, -5)
+        wrap.sprite.move(pla, 0, -4)
     if wrap.K_s in keys:
         wrap.sprite.set_angle(pla, 180)
-        wrap.sprite.move(pla, 0, 5)
+        wrap.sprite.move(pla, 0, 4)
     if wrap.K_a in keys:
         wrap.sprite.set_angle(pla, -90)
-        wrap.sprite.move(pla, -5, 0)
+        wrap.sprite.move(pla, -4, 0)
     if wrap.K_d in keys:
         wrap.sprite.set_angle(pla, 90)
-        wrap.sprite.move(pla, 5, 0)
+        wrap.sprite.move(pla, 4, 0)
